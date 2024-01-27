bab48e439594763049c4cad448128dbe5c71b1bd
kwonkk <ig1108@nate.com>
2024-01-27 20:57:00 +0900

    study

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -2,14 +2,28 @@ package com.fastcampus.ch2;
 
 import java.net.URLEncoder;
 
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
+	
+	@InitBinder
+	public void toDate(WebDataBinder binder) {
+		ConversionService conversionService = binder.getConversionService();
+		System.out.println("conversionService="+conversionService);
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+//		binder.registerCustomEditor(Date.class, new CustomDateEditor(df,false));
+		binder.registerCustomEditor(String[].class, "hobby", new StringArrayPropertyEditor("#"));
+	}
 	
 	@RequestMapping(value="/register/add", method= {RequestMethod.GET, RequestMethod.POST})
 	public String register() {
@@ -18,7 +32,9 @@ public class RegisterController {
 	
 //	@RequestMapping(value="/register/save",method=RequestMethod.POST)
 	@PostMapping("/register/save")	//4.3부터
-	public String save(User user, Model m) throws Exception {
+	public String save(User user, BindingResult result, Model m) throws Exception {
+		System.out.println("result="+result);
+		System.out.println("user="+user);
 		//1. 유효성 검사
 		if(!isValid(user)) {
 			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.","utf-8");
@@ -32,6 +48,6 @@ public class RegisterController {
 	}
  
 private boolean isValid(User user) {
-	return false;
-}
+	return true;
+	}
 }
